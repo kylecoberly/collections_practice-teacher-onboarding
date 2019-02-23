9bd6a0e6b39fb3b5db1cb14436b41ce70cae99e1
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-02-23 23:34:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,6 @@ end
 def sort_array_desc array_of_integers
   array_of_integers.sort { |a, b| b <=> a}
 end
+
+def sort_array_char_count array_of_strings
+  array_of_strings.sort { |a, b| a.length <=> b.length }
